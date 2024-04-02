697dc40e5214daab9e01263d06f99fa15037ff7a
Charlie Morley-Wong <charliemw@garrawarla-1.pawsey.org.au>
2024-04-02 13:35:26 +0800

    Pre Implementation
    - Getting everything ready to play around with parallelisation. Deffs
      not gonna work on the first round, so be prepared to see several other
      commits with a lot of time in between.

--- SmallScale/FacetPropagation/normal_submit.sh
+++ SmallScale/FacetPropagation/normal_submit.sh
@@ -1,10 +1,14 @@
-#!/bin/bash -l
-#
+#!/bin/bash -login
+
 #SBATCH --job-name=roughtest
+#SBATCH --account=mwavcs
+#SBATCH --partition=workq
 #SBATCH --ntasks=1
 #SBATCH --ntasks-per-node=1
 #SBATCH --cpus-per-task=1
 #SBATCH --time=00:00:30
 #SBATCH --output=Outputs/Gary/%x.out
 
+
+#Change the input file to what you need.
 srun ./rough.exe < Inputs/quick_64.in
